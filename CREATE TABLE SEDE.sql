3e97f044cd6a44e3d254bb5c4d5d02181c2567cd
jmoreno182 <jmoreno182@gmail.com>
2023-06-23 17:44:28 -0400

    Creación de la tabla sede

---
+++ CREATE TABLE SEDE.sql
@@ -0,0 +1,5 @@
+CREATE TABLE admbd.sede(
+    id_sede INT AUTO_INCREMENT NOT NULL,
+    sede VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id_sede))
+ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE = utf8_general_ci;

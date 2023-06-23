d53bd6d531379dc83bc84ad15d2a53817b4fa626
jmoreno182 <jmoreno182@gmail.com>
2023-06-22 23:53:56 -0400

    Creacion de tablas o entidades titulo grado

---
+++ CREATE TABLE TITULO GRADO.sql
@@ -0,0 +1,5 @@
+CREATE TABLE admbd.titulo_grado(
+    id_titulo_grado INT AUTO_INCREMENT NOT NULL,
+    titulo_grado VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id_titulo_grado))
+ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE = utf8_general_ci;

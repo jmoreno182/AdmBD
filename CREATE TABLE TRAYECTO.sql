79fe3e7117592dbf1c3148b4ddedc91874ba2d31
jmoreno182 <jmoreno182@gmail.com>
2023-06-22 23:41:55 -0400

    Creación de la tabla trayecto

---
+++ CREATE TABLE TRAYECTO.sql
@@ -0,0 +1,5 @@
+CREATE TABLE admbd.trayecto(
+    id_trayecto INT AUTO_INCREMENT NOT NULL,
+    trayecto VARCHAR(55) NOT NULL,
+    PRIMARY KEY(id_trayecto))
+ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE = utf8_general_ci;

79b946f82aba4dbc2e423cdfd0094603277ec647
jmoreno182 <jmoreno182@gmail.com>
2023-06-22 23:59:13 -0400

    Creación de la tabla o entidad especialidad

---
+++ CREATE TABLE ESPECIALIDAD.sql
@@ -0,0 +1,5 @@
+CREATE TABLE admbd.especialidad(
+    id_especialidad INT AUTO_INCREMENT NOT NULL,
+    especialidad VARCHAR(55) NOT NULL,
+    PRIMARY KEY(id_especialidad))
+ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE = utf8_general_ci;

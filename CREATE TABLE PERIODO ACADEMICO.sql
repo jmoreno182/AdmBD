e0b11e1c9c8219c85bd5bccc97254b38c9550944
jmoreno182 <jmoreno182@gmail.com>
2023-06-22 23:51:07 -0400

    Creacion de tablas o entidades

---
+++ CREATE TABLE PERIODO ACADEMICO.sql
@@ -0,0 +1,5 @@
+CREATE TABLE admbd.periodo_academico(
+    id_periodo_academico INT AUTO_INCREMENT NOT NULL,
+    periodo_academico VARCHAR(55) NOT NULL,
+    PRIMARY KEY(id_periodo_academico))
+ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE = utf8_general_ci;
---
+++ CREATE TABLE UBICACIONES.sql
@@ -0,0 +1,5 @@
+CREATE TABLE admbd.ubicaciones(
+    id_ubicaciones INT AUTO_INCREMENT NOT NULL,
+    ubicaciones VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id_ubicaciones))
+ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE = utf8_general_ci;

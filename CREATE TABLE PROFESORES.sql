3576be3b6ab0d302e97da326bbee872f7a75b7cb
jmoreno182 <jmoreno182@gmail.com>
2023-06-23 17:06:54 -0400

    cambios finales

--- CREATE TABLE PROFESORES.sql
+++ CREATE TABLE PROFESORES.sql
@@ -1,6 +1,6 @@
 CREATE TABLE admbd.profesores(
     id_profesor INT AUTO_INCREMENT NOT NULL,
-    key_titulo INT
-    key_especialidad INT
+    key_titulo INT NOT NUll,
+    key_especialidad INT NOT NULL,
     PRIMARY KEY(id_profesor))
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE = utf8_general_ci;

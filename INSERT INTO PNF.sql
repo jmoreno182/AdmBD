a4ca7040a053cba4b6db9634db27256ced01585f
jmoreno182 <jmoreno182@gmail.com>
2023-06-24 23:36:25 -0400

    Inserción en la entidad padre "pnf"

---
+++ INSERT INTO PNF.sql
@@ -0,0 +1,11 @@
+INSERT INTO admbd.pnf (id_pnf, pnf, desc_pnf) VALUES
+(NULL, 'PNF Administración', 'Es un profesional que debe desempeñarse en tareas propias de la administración en el área contable y de costos industriales.'),
+(NULL, 'PNF Agroalimentación', 'Es un profesional que debe desempeñarse en tareas propias de la Agroalimentación.'),
+(NULL, 'PNF Contaduría Pública', 'Es un profesional que debe desempeñarse en tareas propias de la Contaduría Pública.'),
+(NULL, 'PNF Electricidad', 'Es un profesional que debe desempeñarse en tareas propias de la Electricidad.'),
+(NULL, 'PNF Electrónica', 'Es un profesional que debe desempeñarse en tareas propias de la Electrónica.'),
+(NULL, 'PNF Informática', 'Es un profesional que debe desempeñarse en tareas propias de la Informática.'),
+(NULL, 'PNF Instrumentación y Control', 'Es un profesional que debe desempeñarse en tareas propias de la Instrumentación y Control.'),
+(NULL, 'PNF Mantenimiento', 'Es un profesional que debe desempeñarse en tareas propias del Mantenimiento.'),
+(NULL, 'PNF Mecánica', 'Es un profesional que debe desempeñarse en tareas propias de la Mecánica.'),
+(NULL, 'PNF Sistemas de Calidad y Ambiente', 'Es un profesional que debe desempeñarse en tareas propias de Sistemas de Calidad y Ambiente.');

119a39b4313c388fdde7de72f8a665a2cfd33605
jmoreno182 <jmoreno182@gmail.com>
2023-06-25 09:15:59 -0400

    Insertar maya curricular

---
+++ INSERT INTO MAYA_CURRICULAR.sql
@@ -0,0 +1,7 @@
+INSERT INTO maya_curricular (id_maya, maya_curricular, fech_creacion, key_pnf) VALUES 
+(NULL, '0', current_timestamp(), '1'),
+(NULL, '1', current_timestamp(), '1'),
+(NULL, '0', current_timestamp(), '2'),
+(NULL, '1', current_timestamp(), '2'),
+(NULL, '0', current_timestamp(), '4'),
+(NULL, '1', current_timestamp(), '4');
